d980a39790c10a94999b872db193b68b1050140f
MKToun <matthieu.pavageau@insa-rouen.fr>
2019-02-27 14:03:08 +0100

    process_message server.c

--- DS/MessageQueues/exercise1/server.c
+++ DS/MessageQueues/exercise1/server.c
@@ -1,9 +1,12 @@
 #include <mqueue.h>
 #include <pthread.h>
 #include <stdio.h>
+#include <string.h>
+
 
 #define MAXSIZE 256
 #define TRUE 1
+#define FALSE 0
 
 struct request{
     char key[MAXSIZE];
@@ -15,6 +18,33 @@ struct request{
 pthread_mutex_t mutex_msg;
 int msg_not_copied = TRUE;
 pthread_cond_t cond_msg;
+
+void process_message(struct request *msg){
+    struct request msg_local;
+    mqd_t q_client;
+    int result;
+
+    pthread_mutex_lock(&mutex_msg);
+    memcpy((char *) &msg_local, (char *) &msg, sizeof(struct request));
+
+    msg_not_copied = FALSE;
+
+    pthread_cond_signal(&cond_msg);
+
+    pthread_mutex_unlock(&mutex_msg);
+
+    result = 0;
+
+    q_client = mq_open(msg_local.q_name, O_WRONLY);
+
+    if(q_client == -1)
+        perror("Can't open client queue");
+    else{
+        mq_send(q_client, (char *) &result, sizeof(int), 0);
+        mq_close(q_client);
+    }
+    pthread_exit(0);
+}
 
 int main(void){
     mqd_t q_server;
@@ -28,19 +58,19 @@ int main(void){
 
     q_server = mq_open("SERVER", O_CREAT|O_RDONLY, 0700, &q_attr);
     if(q_server == -1){
-        perror("Can't cerate server queue");
+        perror("Can't create server queue");
         return 1;
     }
 
     pthread_mutex_init(&mutex_msg, NULL);
     pthread_cond_init(&cond_msg, NULL);
-    pthread_attr_init(&q_attr);
+    pthread_attr_init(&t_attr);
 
     pthread_attr_setdetachstate(&t_attr, PTHREAD_CREATE_DETACHED);
 
     while(TRUE){
-        mq_receive(q_server, &msg, sizeof(struct request), 0);
-        pthread_create(&thid, &t_attr, process_message, &msg);
+        mq_receive(q_server, (char *) &msg, sizeof(struct request), 0);
+        pthread_create(&thid, &t_attr, (void (*))process_message, &msg);
 
         pthread_mutex_lock(&mutex_msg);
         while(msg_not_copied)
@@ -48,8 +78,4 @@ int main(void){
         msg_not_copied = TRUE;
         pthread_mutex_unlock(&mutex_msg);
     }
-}
-
-void process_message(struct request *msg){
-    
 }

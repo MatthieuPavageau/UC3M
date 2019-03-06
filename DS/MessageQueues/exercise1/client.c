bc40ed7caa64bbe9d4dd1a1d104142a9dc416400
Darius <darius.graz@gmail.com>
2019-03-06 14:24:35 +0100

    SET VALUE method fixed. GET VALUE method almost working

--- DS/MessageQueues/exercise1/client.c
+++ DS/MessageQueues/exercise1/client.c
@@ -1,26 +1,88 @@
-#include "keys.c"
+#include "keys.h"
+#include <stdlib.h>
+#include <stdio.h>
+//#define MAX_MESSAGES 10
 
 int main()
 {
-    char key[256] = "key1";
-    char value1[256];
-    float value2;
+    
+ // init();
+ // set_value("key1", "test1", 1.1);
+ //get_value("key1", value1, &n/umber);
+    int choice;
+    printf("CLIENT SERVER PROGRAM IMPLEMENTED USING QUEUES\n");
+    printf("OPTION 1 : initialize the system\n"); 
+    printf("OPTION 2 : set value\n"); 
+    printf("OPTION 3 : get value\n"); 
+    printf("OPTION 4 : modify value\n"); 
+    printf("OPTION 5 : delete key\n"); 
+    printf("OPTION 6 : check if an element exist\n"); 
+    printf("OPTION 7 : get the number of elements in a queue\n"); 
+    printf("OPTION 8 : exit program\n");
+    printf("                  \n");
+    printf("Enter your choice:\n"); 
+     
+     
+    
+        scanf("%d", &choice);
+    switch (choice) { 
+    
+    case 1: { 
+        printf("You've chosen option 1\n");
+        init();
+        break; 
+    } 
+    case 2: { 
+        printf("You've chosen option 2\n");
+        int n;
+                char k[100],b[100];
+        float z;
+        printf("Enter how many triplets you want to send to server : \n");
+        scanf("%d", &n);
+       for (int i = 0; i < n ; ++i)
+       {
+            printf("Enter key :\n");
+            scanf("%99s", k);
+            printf("Enter value 1 :\n");
+            scanf("%99s", b);
+            printf("Enter value 2 :\n");
+            scanf("%f", &z);
+            set_value(k, b, z);
+       }
+          
+            
+        break; 
+    } 
+    case 3: { 
+        float value2; 
+        char value1;
+        get_value("qwe", &value1, &value2);
+        break; 
+    } 
+    case 4: { 
+        //modify_value();
+        break; 
+    } 
+    case 5: { 
+        //delete_key();
+        break; 
+    } 
+    case 6: { 
+        //exist();
+        break; 
+    } 
+    case 7: { 
+        //num_items();
+        break; 
+    } 
+    default: 
+        printf("WRONG INPUT\n"); 
+}
 
-    init();
 
-    set_value("key1", "test1", 1.1);
 
-    get_value(key, value1, &value2);
+        
 
-    value2 = 1.2;
 
-    modify_value(key, "modified1", &value2);
-
-    get_value(key, value1, &value2);
-
-    exist(key);
-
-    delete_key(key);
-
-    exist(key);
+    return 0; 
 }
--- DS/MessageQueues/exercise1/keys.c
+++ DS/MessageQueues/exercise1/keys.c
@@ -1,36 +1,218 @@
-#include <mqueue.h>
+#include "keys.h"
+#define MAX_SIZE 256
+#define QUEUE_PERMISSIONS 0660
+#define MAX_MESSAGES 10
+//#define MSG_BUFFER_SIZE MAX_SIZE
+#define SERVER_QUEUE "/SERVER_QUEUE"
+#define CLIENT_QUEUE "/CLIENT_QUEUE"
+
+
+
+
+typedef struct Triplets{
+  char key[MAX_SIZE];
+  char value1[MAX_SIZE];
+  float value2;
+  char q_name[MAX_SIZE];
+  int op_code;
+} Triplets;
+
+ mqd_t q_client;
+ mqd_t q_server;
+
+struct mq_attr attributes_s,attributes_c;
 
 int init()
 {
+    int res;
+   
+    Triplets triplet;
+
+    mq_close(q_client);
+    mq_close(q_server);
+
+    //struct mq_attr attributes_c;
+   //	struct mq_attr attributes_s;
+
+   	attributes_s.mq_maxmsg  = MAX_MESSAGES;
+    attributes_s.mq_msgsize = sizeof(triplet);
+    attributes_c.mq_maxmsg  = MAX_MESSAGES;
+    attributes_c.mq_msgsize = sizeof(int);
+
+
+    if ((q_client = mq_open(CLIENT_QUEUE, O_CREAT|O_RDONLY, 0777, &attributes_c))==-1)
+    {
+      printf("AAA Error mq_open\n");
+      exit(-1);
+    } else
+    {
+      printf("CLIENT QUEUE OPENED (CLIENT SIDE_1)\n");
+    }
+
+   	if ((q_server = mq_open(SERVER_QUEUE, O_WRONLY, 0777, &attributes_s))==-1)
+   	{
+   		perror(" BBB Error mq_open\n");
+   		exit(-1);
+   	} else
+   	{
+   		printf("SERVER QUEUE OPENED (CLIENT SIDE_1)\n");
+   	}
+
+    strcpy( triplet.q_name, CLIENT_QUEUE);
+    triplet.op_code = 1;
+    bzero(triplet.key, MAX_SIZE);
+
+    if (mq_send(q_server, (const char *) &triplet, sizeof(Triplets), 0)== -1)
+    {
+      perror(" CCC mq_send");
+          printf("%s", triplet.key);
+          mq_close(q_server);
+      exit(1);
+    }
+
+    if (mq_receive(q_client, (char *) &res, sizeof(int), 0)== -1)
+    {
+      perror(" DDD mq_recv");
+       printf("%s", triplet.key);
+      mq_close(q_server);
+      exit(1);
+    } else
+    {
+      printf("%d\n", res);
+    }
+    
+    
+    mq_close(q_client);
+    mq_close(q_server);
+
     return 0;
 }
 
-int set_value(char* key, char *value1, float value2)
+int set_value(char* key, char* value1, float value2)
 {
+  
+  Triplets triplet;
+  
+  strcpy( triplet.key, key);
+  strcpy( triplet.value1, value1); 
+  triplet.value2 = value2;
+  triplet.op_code = 2;
+  strcpy( triplet.q_name, CLIENT_QUEUE);
+
+  
+
+  attributes_s.mq_maxmsg  = MAX_MESSAGES;
+  attributes_s.mq_msgsize = sizeof(triplet);
+
+  if ((q_server = mq_open(SERVER_QUEUE, O_WRONLY, 0777, &attributes_s))==-1)
+    {
+      perror(" BBB Error mq_open\n");
+      exit(-1);
+    } else
+    {
+      printf("SERVER QUEUE OPENED (CLIENT SIDE_2)\n");
+    }
+ 
+    if (mq_send(q_server, (const char *) &triplet, sizeof(Triplets), 0)== -1)
+    {
+      perror("mq_send");
+       printf("%s", triplet.key);
+      mq_close(q_server);
+      exit(1);
+    } else
+    {
+      printf("MESSAGE SENT FROM CLIENT TO SERVER (CLIENT SIDE_2)\n");
+    }
+
+  mq_close(q_server);
+  		
     return 0;
 }
 
 int get_value(char* key, char *value1, float *value2)
 {
+
+  Triplets triplet;
+
+  attributes_s.mq_maxmsg  = MAX_MESSAGES;
+  attributes_s.mq_msgsize = sizeof(triplet);
+
+  strcpy( triplet.key, key);
+  strcpy( triplet.value1, value1); 
+  triplet.value2 = *value2;
+  triplet.op_code = 3;
+  strcpy( triplet.q_name, CLIENT_QUEUE);
+
+
+   if ((q_server = mq_open(SERVER_QUEUE, O_WRONLY, 0777, &attributes_s))==-1)
+    {
+      perror(" GGG Error mq_open\n");
+      exit(-1);
+    } else
+    {
+      printf("SERVER QUEUE OPENED (CLIENT SIDE_3)\n");
+    }
+
+    if ((q_client = mq_open(CLIENT_QUEUE, O_RDONLY, 0777, &attributes_s))==-1)
+    {
+      perror(" GGG Error mq_open\n");
+      exit(-1);
+    } else
+    {
+      printf("SERVER QUEUE OPENED (CLIENT SIDE_3)\n");
+    }
+
+     if (mq_send(q_server, (const char *) &triplet, sizeof(Triplets), 0)== -1)
+    {
+      perror("mq_send");
+       
+      mq_close(q_server);
+      exit(1);
+    } else
+    {
+      printf("MESSAGE SENT FROM CLIENT TO SERVER (CLIENT SIDE_3)\n");
+    }
+
+    if((mq_receive(q_client, (char *) &triplet, sizeof(Triplets), 0)==-1))
+    {
+          perror("ERROR (CLIENT SIDE_3) -> mq_receive");
+          mq_close(q_client);
+          exit(1);
+    } else
+    {
+      printf("RECEIVED KEY %s\n", triplet.key );
+      printf("RECEIVED VALUE_1 %s\n", triplet.value1);
+      printf("RECEIVED VALUE_2 %f\n", triplet.value2);
+
+    }
+
+      mq_close(q_server);
+               
+
     return 0;
 }
 
 int modify_value(char* key, char *value1, float *value2)
 {
+//	int mq_setattr(mqd_t mqdes, struct mq_attr *qstat,struct mq_attr *oldmqstat);
     return 0;
 }
 
 int delete_key(char* key)
 {
+  // RECEIVE and set to null
     return 0;
 }
 
 int exist(char* key)
 {
+    //  int mq_getattr (mqd_t mqdes, struct mq_attr *qstat)
+    // 0 yes exist -1 no
     return 0;
 }
 
 int num_items()
 {
+
     return 0;
 }
